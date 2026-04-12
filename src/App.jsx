3d38687f59c79089c5ace91b9cd07bb7dea445da
BernardSDev <sb.bernardsam.dev@gmail.com>
2026-04-12 19:35:33 +0000

    Add useState logic to control switch toggle behavior

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import {useState} from "react";
+
 function App() {
+    const [isLightOn, setIsLightOn] = useState(false);
+
+    function handleSwitch() {
+        setIsLightOn(!isLightOn);
+    }
+
     return (
         <div className="main-container">
             <div className="control">
@@ -9,11 +17,16 @@ function App() {
                     Learn how React state controls visibility by building simple show and hide (toggle) screens in
                     real-world interfaces.
                 </p>
-                <button className="control-button">
+                <button
+                    onClick = { handleSwitch }
+                    className="control-button"
+                >
                     Switch on
                 </button>
             </div>
-            <div className="screen">Screen</div>
+            <div className="screen">
+                { isLightOn ? <h1>Light is On</h1> : <h1>Light is Off</h1> }
+            </div>
         </div>
     )
 }
