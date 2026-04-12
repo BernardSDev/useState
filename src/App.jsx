f11740ce39d47265b2e8f2fdb806e2e716395580
BernardSDev <sb.bernardsam.dev@gmail.com>
2026-04-12 19:13:30 +0000

    Set up the application with a control section for UI interaction

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 function App() {
     return (
-        <h1>useState</h1>
+        <div className="main-container">
+            <div className="control">
+                <h1 className="control-heading">
+                    Understanding useState: Turning UI Elements On and Off
+                </h1>
+                <p className="control-subheading">
+                    Learn how React state controls visibility by building simple show and hide (toggle) screens in
+                    real-world interfaces.
+                </p>
+                <button className="control-button">
+                    Switch on
+                </button>
+            </div>
+            <div className="screen">Screen</div>
+        </div>
     )
 }
 
