910a2529e39b270d744bdf81a7cf3489bc4a338c
BernardSDev <sb.bernardsam.dev@gmail.com>
2026-04-19 19:08:24 +0000

    Separate light switch feature into its own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,9 @@
-import {useState} from "react";
+import LightSwitch from "./components/LightSwitch.jsx";
 
 function App() {
-    const [isLightOn, setIsLightOn] = useState(false);
-
-    function handleSwitch() {
-        setIsLightOn(!isLightOn);
-    }
-
     return (
-        <div className="main-container">
-            <div className="control">
-                <h1 className="control-heading">
-                    Understanding useState: Turning UI Elements On and Off
-                </h1>
-                <p className="control-subheading">
-                    Learn how React state controls visibility by building simple show and hide (toggle) screens in
-                    real-world interfaces.
-                </p>
-                <button
-                    onClick = { handleSwitch }
-                    style={{ backgroundColor: isLightOn ? "#22c55e" : "#B47753", color: "white" }}
-                    className="control-button"
-                >
-                    { isLightOn ? "Switch off" : "Switch on" }
-                </button>
-            </div>
-            <div
-                className="screen"
-                style={{
-                    background: isLightOn ? "#fff7c2" : "#111827",
-                    color: isLightOn ? "#1f2937" : "#f9fafb",
-                }}
-            >
-                { isLightOn ? <h1>Light is On</h1> : <h1>Light is Off</h1> }
-            </div>
+        <div>
+            <LightSwitch />
         </div>
     )
 }
